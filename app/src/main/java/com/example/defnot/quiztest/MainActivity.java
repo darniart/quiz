4f2276d5c7ca2f337b348ecb49d8866fdb173f21
DefNot <damian.buksowicz@gmail.com>
2020-01-08 00:51:58 +0100

    Category view

---
+++ app/src/main/java/com/example/defnot/quiztest/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.defnot.quiztest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.example.defnot.quiztest.Adapter.CategoryAdapter;
+import com.example.defnot.quiztest.Common.SpaceDecoration;
+import com.example.defnot.quiztest.DBHelper.DBHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recycler_category;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("QuizAplication DefNot");
+        setSupportActionBar(toolbar);
+
+        recycler_category = (RecyclerView)findViewById(R.id.recycler_category);
+        recycler_category.setHasFixedSize(true);
+        recycler_category.setLayoutManager(new GridLayoutManager(this, 2));
+
+
+        // Screen height
+        CategoryAdapter adapter = new CategoryAdapter(MainActivity.this,DBHelper.getInstance(this).getAllCategories());
+        int spaceInPixel = 4;
+        recycler_category.addItemDecoration(new SpaceDecoration(spaceInPixel));
+        recycler_category.setAdapter(adapter);
+    }
+}
