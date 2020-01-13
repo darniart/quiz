15bc31b7b4719628fe2dad5ee65096f5e419fb1e
DefNot <damian.buksowicz@gmail.com>
2020-01-13 04:05:51 +0100

    Online mode others

---
+++ app/src/main/java/com/example/defnot/quiztest/Interface/MyCallback.java
@@ -0,0 +1,9 @@
+package com.example.defnot.quiztest.Interface;
+
+import com.example.defnot.quiztest.Model.Question;
+
+import java.util.List;
+
+public interface MyCallback {
+    void  setQuestionList(List<Question> questionList);
+}
